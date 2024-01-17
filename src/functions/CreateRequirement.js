cad1ee3a6acf9576dd0c70241b24da576e2c3b42
Md Samir Ansari <samir.ansari@quickelf.com>
2024-01-17 17:59:17 +0530

    merge changes

--- src/functions/CreateRequirement.js
+++ src/functions/CreateRequirement.js
@@ -1,4 +1,32 @@
 const { app } = require('@azure/functions');
+const sql = require('mssql');
+const server = 'uatx-qa-sqlserver.database.windows.net';
+const database = 'uatx-qa-sqldb';
+const port = 1433;
+const type = 'azure-active-directory-default';
+
+  const config = {
+    server,
+    port,
+    database,
+    authentication: {
+        type: type,
+    },
+    options: {
+        encrypt: true,
+        // clientId: "3b7ef9f3-48fc-4d29-a9f8-3de02b79dac0"  // <----- user-assigned managed identity        
+    }
+  };
+
+  async function connect() {
+    try {
+        var poolconnection = await sql.connect(config);
+        var query = await poolconnection.request().query(`SELECT * FROM react.employees`);
+        return query;
+    } catch (error) {
+        console.error(error);
+    }
+  }
 
 app.http('CreateRequirement', {
     methods: ['GET', 'POST'],
@@ -10,6 +38,10 @@ app.http('CreateRequirement', {
         var requirement = request.body;
         var data = JSON.stringify(requirement);
         console.log(`12--> ${data} DATA fetched successfully!`);
+        var poolconnection = await sql.connect(config);
+        var query = await poolconnection.request().query(`INSERT INTO react.employees (user_name,user_id,salary,city) VALUES('MoSamir','5',20000,${JSON.stringify(request.body)})`);
+        console.log(`43--> ${JSON.stringify(query)}`);
         return { body: `Hello, ${name}!, ${data}` };
     }
+    
 });
