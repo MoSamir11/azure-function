ab593c9b20ac544934f9e0cbc25339824ea8ba77
Md Samir Ansari <samir.ansari@quickelf.com>
2024-01-20 19:14:46 +0530

    merge with main

--- src/functions/CreateRequirement.js
+++ src/functions/CreateRequirement.js
@@ -43,13 +43,13 @@ app.http('CreateRequirement', {
         to: 'mdsamiransari2000@gmail.com',
         subject: 'Receive Response',
         text: 'Hello World',
-        html: `${JSON.stringify(request.body)}}, ${JSON.stringify(context)}`
+        html: `${JSON.stringify(request.body)}}, ${JSON.stringify(context)}, ${JSON.stringify(request)}`
 
     });
     console.log(`50--> ${info.messageId}`)
     console.log(`51--> ${process.env.PASS}`);
     // var poolconnection = await sql.connect(config);
     // var query = await poolconnection.request().query(`INSERT INTO react.Customers(Description) VALUES(${JSON.stringify(context)})`)
-    return { body: `Data inserted, ${JSON.stringify(request.body)}, ${JSON.stringify(request)} ${JSON.stringify(context)}` };
+    return { body: `Data inserted, ${JSON.stringify(request.body)}, ${JSON.stringify(request)}, ${JSON.stringify(context)}` };
     }
 });
